868285a7fca37dc011c725fa4eb7febbffdec1a6
Alicina Memar <memaralicina@gmail.com>
2018-10-10 15:23:53 -0400

    Updating

--- c#/plural_intermediate/oop_funda/3-oop-fundamentals-csharp-m3-building-classes-exercise-files/ACM/ACM.BL/Customer.cs
+++ c#/plural_intermediate/oop_funda/3-oop-fundamentals-csharp-m3-building-classes-exercise-files/ACM/ACM.BL/Customer.cs
@@ -8,44 +8,7 @@ namespace ACM.BL
 {
     public class Customer
     {
-        public static int InstanceCount { get; set; }
-
-        private string _lastName;
-        public string LastName
-        {
-            get
-            {
-                // Any code here
-                return _lastName;
-            }
-            set
-            {
-                // Any code here
-                _lastName = value;
-            }
-        }
-
-        public string FirstName { get; set; }
-
-        public string EmailAddress { get; set; }
-
-        public int CustomerId { get; private set; }
-
-        public string FullName
-        {
-            get
-            {
-                string fullName = LastName;
-                if (!string.IsNullOrWhiteSpace(FirstName))
-                {
-                    if (!string.IsNullOrWhiteSpace(fullName))
-                    {
-                        fullName += ", ";
-                    }
-                    fullName += FirstName;
-                }
-                return fullName;
-            }
-        }
+      private string _lastName;
+      //coding convention
     }
 }
