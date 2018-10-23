d8c61fb4489c69549aaa46017ddf0a3c40b1c2a7
Alicina Memar <amemar@STRASZ.COM>
2018-10-23 16:51:01 -0400

    Working on Csharp interfaces intermediate course

---
+++ c#/plural_intermediate/interfaces/Polygons/Polygons.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Polygons.Library
+{
+    public class ConcreteRegularPolygon
+    {
+        public int NumberOfSides { get; set; }
+
+        //automatic property^
+        //private int _numberOfSides;
+
+        //public int NumberOfSides
+        //{
+        //    get { return _numberOfSides; }
+        //    set { _numberOfSides = value; }
+        //}
+        //full property but equivelant
+
+        public int SideLength { get; set; }
+
+        public ConcreteRegularPolygon(int sides, int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+        }
+
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
