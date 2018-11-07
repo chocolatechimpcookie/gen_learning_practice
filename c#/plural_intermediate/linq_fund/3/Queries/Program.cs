19d4d11e5853c98e4791d1947050afcb28cc266d
Alicina Memar <amemar@STRASZ.COM>
2018-11-07 13:18:14 -0500

    updates

--- c#/plural_intermediate/linq_fund/3/Queries/Program.cs
+++ c#/plural_intermediate/linq_fund/3/Queries/Program.cs
@@ -11,12 +11,14 @@ namespace Queries
             var movies = new List<Movie>
             {
                 new Movie { Title="The Dark Knight", Rating=8.9f, Year = 2008 },
-                new Movie { Title="Star Wars V", Rating = 8.7f, Year = 1980 },
-                new Movie { Title ="Casablanca", Rating=8.5f, Year = 1942 },
-                new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 }
-            };
+                new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 },
+				new Movie { Title ="Casablanca", Rating=8.5f, Year = 1942 },
+				new Movie { Title="Star Wars V", Rating = 8.7f, Year = 1980 }
+
+			};
 
 			var query = movies.Where(mov => mov.Year > 2000);
+
 			//var query = movies.Filter(mov => mov.Year > 2000);
 			//using MyLinq Filter
 
