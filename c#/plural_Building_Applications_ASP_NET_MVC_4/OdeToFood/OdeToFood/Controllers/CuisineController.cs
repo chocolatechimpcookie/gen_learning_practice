265ec39bd446f0d518448438e86b6819a75d0ee4
Alicina Memar <memaralicina@gmail.com>
2018-11-30 16:44:05 -0500

    updating

--- c#/plural_Building_Applications_ASP_NET_MVC_4/OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ c#/plural_Building_Applications_ASP_NET_MVC_4/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -8,13 +8,12 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
-        //
-        // GET: /Cuisine/
-
-        public ActionResult Search(string name)
+        [Authorize]
+        public ActionResult Search(string name = "french")
         {
             var message = Server.HtmlEncode(name);
-            return Content(name);
+            return Content(message);
+
         }
 
     }
