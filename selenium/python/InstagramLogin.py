55b69ccbcdd22d24e3d35c6b736d19cfe3a8bfe7
Cina <memaralicina@gmail.com>
2019-10-23 08:11:39 -0400

    update

--- selenium/python/InstagramLogin.py
+++ selenium/python/InstagramLogin.py
@@ -5,6 +5,7 @@ import time
 # user data input
 username_input = raw_input()
 password_input = raw_input()
+profilename= raw_input()
 print(username_input)
 driver = webdriver.Firefox()
 
@@ -22,20 +23,30 @@ password_field = driver.find_element_by_xpath("//input[@name= 'password']")
 password_field.send_keys(password_input)
 submit_login = driver.find_element_by_xpath("//button[@type='submit']")
 submit_login.click()
-
+# testrencemcexam
 
 # If the notification button comes up -> click not Now
-# its not picking up the notification buttons 
-time.sleep(1)
-notification_button_not_now = driver.find_element_by_xpath("//button[text()='Not Now']")
+# its not picking up the notification buttons
+# notification_button_not_now = driver.find_element_by_xpath("//button[text()='Not Now']")
+time.sleep(3)
+try:
+    notification_button_not_now = driver.find_element_by_xpath("//button[text()='Not Now']")
+    notification_button_not_now.click()
+except:
+    print("Notification isn't there ")
 
-# try:
-    # notification_button_not_now = driver.find_element_by_xpath("//button[text()='Not Now']")
-    # notification_button_not_now.click()
-# except:
-    # print("Notification isn't there ")
-
+# I have to have followers to check by story
 # check loggedin
 # story_element = driver.find_element_by_xpath("//div[text()='Stories']")
 # driver.assertEqual()
 # print(story_element);
+
+# profile_button = driver.find_element_by_xpath("//svg[@aria-label='Profile']")
+profilelocator = "//a[@href='/"+profilename+"/']"
+profile_button = driver.find_element_by_xpath(profilelocator)
+profile_button.click()
+time.sleep(3)
+edit_profile = driver.find_element_by_xpath("//button[text()='Edit Profile']")
+# driver.assertEqual("Edit Profile", edit_profile.getAttribute("innerText"))
+# driver.assertEqual("Edit Profile", edit_profile.getAttribute("textContent"))
+assert "Edit Profile" in edit_profile.getAttribute("textContent")
